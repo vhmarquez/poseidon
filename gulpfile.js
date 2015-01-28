6b1c76cd047abb4c67de19b797b9a50e1de9e04b
Victor <victor@ascentus.com>
2015-01-28 01:05:57 -0500

    Set up gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+///////////////////////////////////////////////
+// NODE MODULES
+// =========================
 
+var	gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	rename = require('gulp-rename'),
+	compass = require('gulp-compass'),
+	minifyCSS = require('gulp-minify-css');
+
+// Default gulp tasks
 gulp.task('default', function() {
-  // place code for your default task here
+
+});
+
+// Compile SASS and Minify CSS
+gulp.task('compass', function() {
+	gulp.src('architecture/sass/**/*.sass')
+		.pipe(compass({
+			css: 'architecture/css',
+			sass: 'architecture/sass'
+		}))
+		.pipe(gulp.dest('architecture/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('architecture/css'));
+});
+
+// Watch Task
+gulp.task('watch', function(){
+	gulp.watch('architecture/sass/*.sass', ['compass']);
 });
