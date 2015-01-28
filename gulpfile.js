58a649610b042a4313e0984f4f5df58e31b4561f
Victor <victor@ascentus.com>
2015-01-28 13:23:16 -0500

    Set up folders and Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var	gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	rename = require('gulp-rename'),
 	compass = require('gulp-compass'),
-	minifyCSS = require('gulp-minify-css');
+	minifyCSS = require('gulp-minify-css'),
+	uglify = require('gulp-uglify');
 
 // Default gulp tasks
 gulp.task('default', function() {
@@ -15,12 +16,28 @@ gulp.task('default', function() {
 
 // Compile SASS and Minify CSS
 gulp.task('compass', function() {
-	gulp.src('architecture/sass/**/*.sass')
+	gulp.src('temp/sass/**/*.sass')
 		.pipe(compass({
-			css: 'architecture/css',
-			sass: 'architecture/sass'
+			css: 'temp/css',
+			sass: 'temp/sass'
 		}))
-		.pipe(gulp.dest('architecture/css'))
+		.pipe(gulp.dest('temp/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('architecture/css'));
+});
+
+// Uglify JS
+gulp.task('compress', function(){
+	gulp.src('temp/js/**/*.js')
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('architecture/js'));
+});
+
+// Minify all CSS
+gulp.task('minify-css', function(){
+	gulp.src('temp/css/*.css')
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('architecture/css'));
@@ -28,5 +45,6 @@ gulp.task('compass', function() {
 
 // Watch Task
 gulp.task('watch', function(){
-	gulp.watch('architecture/sass/*.sass', ['compass']);
+	gulp.watch('temp/sass/**/*.sass', ['compass']);
+	gulp.watch('temp/js/**/*.js', ['compress']);
 });
