3498d8bbd58b8b54e32aca584d4a8ada21335364
Victor <victor@ascentus.com>
2015-01-29 01:41:49 -0500

    new animations, and new menu.

--- temp/js/main.js
+++ temp/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$('.menu-icon').on('click', function(){
 		$('#application').toggleClass('active-menu');
+		$(this).children('i').toggleClass('click-fadein');
+		$(this).children('i').toggleClass('click-fadeout');
 	});
 });
