c31b8a7299e8f2bd7ea64f545ed97b6068d643ce
Victor Marquez <victormarquez.dev@gmail.com>
2015-01-29 23:37:37 -0500

    Switch profile functionality.

--- temp/js/main.js
+++ temp/js/main.js
@@ -6,4 +6,39 @@ $(document).ready(function() {
 		$('.menu-icon').children('i').toggleClass('click-fadeout');
 	});
 
+	$('.navigation-header-switch').on('click', function(){
+
+		var 	switchHeight = $('#profileSwitch').height();
+			switchHeight = switchHeight + 61;
+
+			console.log(switchHeight);
+
+		if($(this).hasClass('active')){
+			var 	profileHeight = 61;
+				i = 0;
+
+			$(this).parent('#profileSwitch').children('.navigation-header-switch').each(function() {
+				i++;
+			});
+
+			console.log(switchHeight);
+
+			if(switchHeight < 62){
+				$('#profileSwitch').height((profileHeight*i) - 61);
+			} else if(switchHeight > 61) {
+				$('#profileSwitch').css('height', '61px');
+			}
+
+		} else {
+
+			$(this).siblings('.navigation-header-switch').removeClass('active');
+			$(this).addClass('active');
+			$('#profileSwitch').css('height', '61px');
+
+		}
+
+	});
+
+
+
 });
